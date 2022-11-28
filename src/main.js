7c4759da6b6c3593800507ecfecbb62e97e69e14
Latrevion <xiaopikaqiu@outlook.com>
2022-11-28 11:18:03 +0800

    调整eslint规则

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import store from "./store";
 import Vuex from "vuex";
 import { Button } from "ant-design-vue";
 Vue.config.productionTip = false;
 Vue.use(Button);
+Vue.use(Vuex);
 
 new Vue({
   router,
